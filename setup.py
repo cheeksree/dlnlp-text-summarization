91c8a9fcca22ec15ff3ac2d2c566c2cfc39832ad
cheeksree <cheeksree@gmail.com>
2025-06-27 23:14:51 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup,find_packages
 
 setup(name="dlnlp", version="1.0",
-      description="NLP deep learning examples",
-      long_description="NLP deep learning examples",
+      description="NLP deep learning Text Summarization ",
+      long_description="Text Summarization",
       url="https://github.com/cheeksree/dlnlp/",
       author="sree",
       author_email="cheeksree@gmail.com",
